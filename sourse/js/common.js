e42be28ae6d2f2787fa59ef587dec3232443ee28
Pavel <wol1414@gmail.com>
2020-03-03 21:49:06 +0300

    add note page

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -114,7 +114,7 @@ const JSCCommon = {
 				// width: 'auto',
 				// width: th.find(".select2-results__options"),
 				allowClear: false,
-		// dropdownAutoWidth: true
+				// dropdownAutoWidth: true
 			});
 		})
 	},
@@ -136,18 +136,18 @@ const JSCCommon = {
 			$("body").prepend(`<p   class="browsehappy container">К сожалению, вы используете устаревший браузер. Пожалуйста, <a href="http://browsehappy.com/" target="_blank">обновите ваш браузер</a>, чтобы улучшить производительность, качество отображаемого материала и повысить безопасность.</p>`)
 		}
 	},
-	tooltip(){
+	tooltip() {
 		$('[data-toggle="tooltip"]').tooltip();
 	},
-	
-	player(){
-		const players = Array.from(document.querySelectorAll('.audio-js')).map(p => new Plyr(p,{
-			speed: 
-				{ selected: 1, options: [  0.75, 1, 1.25, 1.5] }
-		 ,
+
+	player() {
+		const players = Array.from(document.querySelectorAll('.audio-js')).map(p => new Plyr(p, {
+			speed:
+				{ selected: 1, options: [0.75, 1, 1.25, 1.5] }
+			,
 		}));
 	},
-	dragDrop(){
+	dragDrop() {
 		$(".droppable-section").each(function () {
 			let th = $(this);
 			let dragEl = th.find(".drag-element-js");
@@ -161,13 +161,13 @@ const JSCCommon = {
 				helper: "clone",
 				snapMode: "inner"
 			});
-	
+
 			dropEl.droppable({
 				// greedy: true,
 				containment: th,
 				// cursor: "move",
 				accept: dragEl,
-	
+
 				// tolerance: "fit",
 				drop: function (event, ui) {
 					$(ui.draggable).addClass('drug-in');
@@ -177,12 +177,12 @@ const JSCCommon = {
 						.text(ui.draggable.text())
 						.droppable("disable");
 				},
-	
+
 				out: function (event, ui) {
 					DrafEll($(this), ui.draggable);
 				},
 			});
-	
+
 			// удалить значение / вернуть на место 
 			function DrafEll(drop, drag) {
 				$(drag).removeClass('drug-in')
@@ -200,224 +200,224 @@ const JSCCommon = {
 				DrafEll($(this), `[data-name="${thData}"]`);
 			})
 		})
-	
+
 	},
-	sortWords(){
-		$(".sortable-section").each(function(){
+	sortWords() {
+		$(".sortable-section").each(function () {
 			let _this = $(this);
-			let line =  _this.find(".line-block");
+			let line = _this.find(".line-block");
 			let elem = _this.find(".copy-element-js");
 			let valideCLass = "valid-block";
 			let invalideCLass = "invalid-block";
-		 
-			
-			_this.on('mouseleave','.copy-element-js:not(.active)', function(){
-			 
-					$(this).removeClass(`${invalideCLass}`).removeClass(`${valideCLass}`); 
-			}) 
-		
-			_this.on("click",'.copy-element-js:not(.active)', function(){
-				let valid  = $(this).data("valid");
-				if (valid){ 
+
+
+			_this.on('mouseleave', '.copy-element-js:not(.active)', function () {
+
+				$(this).removeClass(`${invalideCLass}`).removeClass(`${valideCLass}`);
+			})
+
+			_this.on("click", '.copy-element-js:not(.active)', function () {
+				let valid = $(this).data("valid");
+				if (valid) {
 					$(this).addClass(`${valideCLass}`);
 				}
 				else {
 					$(this).addClass(`${invalideCLass}`);
-					
+
 				}
-				$(this).addClass(`active `).clone().appendTo(line);  
-						setTimeout(() => {
-							
-							$(this).removeClass(valideCLass).removeClass(invalideCLass);
-						}, 200);
-				 
-			}) 
-			_this.on("click",'.copy-element-js.active', function(){
+				$(this).addClass(`active `).clone().appendTo(line);
+				setTimeout(() => {
+
+					$(this).removeClass(valideCLass).removeClass(invalideCLass);
+				}, 200);
+
+			})
+			_this.on("click", '.copy-element-js.active', function () {
 				var elemData = $(this).data("name");
 				_this.find(".line-block").find(`[data-name="${elemData}"]`).remove();
-			 
+
 				$(`[data-name="${elemData}"]`).removeClass("active");
 			});
 		})
 	},
-	sortBlock(){
-			// соеденить блоки линией
-	$(".row-two").each(function () {
-		var th = $(this);
-		let flag = true,
-			line = th.find(".line"),
-			x, y,
-			atan2;
-		let valideCLass = "valid-block";
-		let invalideCLass = "invalid-block";
-		let ParentLeft = th.offset().left;
-		let ParentTop = th.offset().top;
+	sortBlock() {
+		// соеденить блоки линией
+		$(".row-two").each(function () {
+			var th = $(this);
+			let flag = true,
+				line = th.find(".line"),
+				x, y,
+				atan2;
+			let valideCLass = "valid-block";
+			let invalideCLass = "invalid-block";
+			let ParentLeft = th.offset().left;
+			let ParentTop = th.offset().top;
 
 
-		th.on('click', ".input-variant--left:not(.disabled):not(" + valideCLass + ")", function () {
-			let thInputLeft = $(this);
-			if (!thInputLeft.hasClass(valideCLass)) {
-				thInputLeft.find(".line").addClass('active');
-				thInputLeft.addClass('active').siblings().removeClass('active').find(".line").removeClass('active');
-				th.find('.input-variant--right:not(' + valideCLass + ')').removeClass('disabled');
-				th.on('mouseenter', '.input-variant--right:not(' + valideCLass + ')', function () {
-					let thInputLeft = th.find(".input-variant--left.active");
-					let thInputRight = $(this);
-					let line = thInputLeft.find(".line.active")
-					if (thInputLeft.hasClass("active") && !thInputLeft.hasClass(valideCLass)) {
-						getPosition(thInputLeft, thInputRight, line);
-					}
-					// $(".line:not(.valid)").removeAttr("style")
-				})
-			}
-		})
-
-
-		th.on('click', '.input-variant--right:not(' + valideCLass + ')', function () {
-			let thInputLeft = th.find(".input-variant--left.active");
-			let thInputRight = $(this);
-			let line = thInputLeft.find(".line.active")
-			thInputRight.addClass('active');
-
-			if (!thInputLeft.hasClass(valideCLass)) {
-				getPosition(thInputLeft, thInputRight, line);
-
-				getValid(thInputLeft, thInputRight, line);
-
-				th.off('mouseleave mouseenter', thInputRight.hasClass(valideCLass));
-			}
-
-		})
-
-
-		th.on('mouseleave', '.input-variant--right:not(.disabled):not(' + valideCLass + ')', function () {
-			let thInputLeft = th.find(".input-variant--left.active");
-			if (!$(this).hasClass(valideCLass) || !$(this).hasClass("invalid-block")) {
-
-				thInputLeft.find('.line').attr('style', '');
-			}
-		})
-
-		// получить позицию для линиии
-		function getPosition(elem1, elem2, line) {
-			if (flag) {
-
-				let widthOne = elem1.width() || null;
-				let heightOne = elem1.height() || null;
-				let elem1Left = elem1.offset().left || null;
-				let elem2Left = elem2.offset().left || null;
-				let widthTwo = elem2.width();
-				let left = elem1 + widthOne;
-				let top = heightOne / 2;
-				let p = Math.PI;
-				if( elem1Left){
-
-					x = -elem1Left + (elem2Left - widthTwo),
-					y = -elem1.offset().top + (elem2.offset().top),
-					atan2 = (180 / p) * Math.atan2(y, x);
-					line.css({
-						width: Math.sqrt(x * x + y * y), 
-						opacity: 1,
-					'-webkit-transform': 'rotate(' + atan2 + 'deg)',
-					'-moz-transform': 'rotate(' + atan2 + 'deg)',
-					'-ms-transform': 'rotate(' + atan2 + 'deg)',
-					'-o-transform': 'rotate(' + atan2 + 'deg)',
-					'transform': 'rotate(' + atan2 + 'deg)',
-				}).addClass("bg-primary");
-			}
-				
-				
-			}
-		}
-		// скрыть линию
-		function reset(line) {
-			if (flag) {
-				line.css({
-					opacity: 0,
-				});
-			}
-		}
-
-
-		// праверить на правильность ответ   и изменить позицию
-		function getValid(elem1, elem2, line) {
-			if (elem1.data("id") == elem2.data("id")) {
-				elem1.addClass(valideCLass).removeClass('active');
-				elem2.addClass(valideCLass).removeClass('active');
-				elem2.siblings().addClass('disabled');
-				line.addClass("valid");
-				th.off('click mouseenter mouseleave', elem1);
-				th.off('click mouseenter mouseleave', elem2);
-				function elemPosition(elem) {
-					let parent = elem.parent();
-					let elemH = elem.height();
-					let elemTop = parent.height() - elemH;
-					parent.height(parent.height());
-
-					let promise = new Promise(function (resolve, reject) {
-						line.css({
-							width: 0,
-						})
-						elem.after(`<div class="remove-div" style="height:${elemH}px; margin-bottom:1rem"></div>`);
-						elem.css({
-							"top": elem.position().top,
-							"position": "absolute",
-							"z-index": "10",
-						});
-					})
-					function getDown() {
-						elem.animate({
-							"top": elemTop - 16,
-						}, 1000);
-						return false;
-					} 
-					promise.then(getDown()); 
-					promise.then($(".remove-div").animate({ "height": 0, "margin-bottom": 0 }, 1000, function () {
-						elem.removeAttr('style').appendTo(parent);
-						if (elem.hasClass("input-variant--left")) {
-						 
-							line.css({
-								width: 100,
-								'-webkit-transform': 'rotate(' + 0 + 'deg)',
-								'-moz-transform': 'rotate(' + 0 + 'deg)',
-								'-ms-transform': 'rotate(' + 0 + 'deg)',
-								'-o-transform': 'rotate(' + 0 + 'deg)',
-								'transform': 'rotate(' + 0 + 'deg)',
-							})
+			th.on('click', ".input-variant--left:not(.disabled):not(" + valideCLass + ")", function () {
+				let thInputLeft = $(this);
+				if (!thInputLeft.hasClass(valideCLass)) {
+					thInputLeft.find(".line").addClass('active');
+					thInputLeft.addClass('active').siblings().removeClass('active').find(".line").removeClass('active');
+					th.find('.input-variant--right:not(' + valideCLass + ')').removeClass('disabled');
+					th.on('mouseenter', '.input-variant--right:not(' + valideCLass + ')', function () {
+						let thInputLeft = th.find(".input-variant--left.active");
+						let thInputRight = $(this);
+						let line = thInputLeft.find(".line.active")
+						if (thInputLeft.hasClass("active") && !thInputLeft.hasClass(valideCLass)) {
+							getPosition(thInputLeft, thInputRight, line);
 						}
+						// $(".line:not(.valid)").removeAttr("style")
+					})
+				}
+			})
 
-					}));
+
+			th.on('click', '.input-variant--right:not(' + valideCLass + ')', function () {
+				let thInputLeft = th.find(".input-variant--left.active");
+				let thInputRight = $(this);
+				let line = thInputLeft.find(".line.active")
+				thInputRight.addClass('active');
+
+				if (!thInputLeft.hasClass(valideCLass)) {
+					getPosition(thInputLeft, thInputRight, line);
+
+					getValid(thInputLeft, thInputRight, line);
+
+					th.off('mouseleave mouseenter', thInputRight.hasClass(valideCLass));
+				}
+
+			})
 
 
+			th.on('mouseleave', '.input-variant--right:not(.disabled):not(' + valideCLass + ')', function () {
+				let thInputLeft = th.find(".input-variant--left.active");
+				if (!$(this).hasClass(valideCLass) || !$(this).hasClass("invalid-block")) {
 
-				};
+					thInputLeft.find('.line').attr('style', '');
+				}
+			})
 
-				setTimeout(() => {
-					elemPosition(elem1);
-					elemPosition(elem2);
-				}, 500);
+			// получить позицию для линиии
+			function getPosition(elem1, elem2, line) {
+				if (flag) {
+
+					let widthOne = elem1.width() || null;
+					let heightOne = elem1.height() || null;
+					let elem1Left = elem1.offset().left || null;
+					let elem2Left = elem2.offset().left || null;
+					let widthTwo = elem2.width();
+					let left = elem1 + widthOne;
+					let top = heightOne / 2;
+					let p = Math.PI;
+					if (elem1Left) {
+
+						x = -elem1Left + (elem2Left - widthTwo),
+							y = -elem1.offset().top + (elem2.offset().top),
+							atan2 = (180 / p) * Math.atan2(y, x);
+						line.css({
+							width: Math.sqrt(x * x + y * y),
+							opacity: 1,
+							'-webkit-transform': 'rotate(' + atan2 + 'deg)',
+							'-moz-transform': 'rotate(' + atan2 + 'deg)',
+							'-ms-transform': 'rotate(' + atan2 + 'deg)',
+							'-o-transform': 'rotate(' + atan2 + 'deg)',
+							'transform': 'rotate(' + atan2 + 'deg)',
+						}).addClass("bg-primary");
+					}
+
+
+				}
 			}
-			else {
-				elem1.addClass(invalideCLass);
-				elem2.addClass(invalideCLass);
-				line.addClass("invalid");
-				elem2.siblings().addClass('disabled');
-				setTimeout(() => {
-					elem1.removeClass(invalideCLass);;
-					elem2.siblings().removeClass('disabled');
-					elem2.removeClass(invalideCLass).removeClass("active ");
-					elem2.siblings().removeClass('disabled');
-					line.removeClass("invalid").attr('style', '');
-					flag = true;
-					// reset(line);
-				}, 1000);
+			// скрыть линию
+			function reset(line) {
+				if (flag) {
+					line.css({
+						opacity: 0,
+					});
+				}
 			}
 
-		}
+
+			// праверить на правильность ответ   и изменить позицию
+			function getValid(elem1, elem2, line) {
+				if (elem1.data("id") == elem2.data("id")) {
+					elem1.addClass(valideCLass).removeClass('active');
+					elem2.addClass(valideCLass).removeClass('active');
+					elem2.siblings().addClass('disabled');
+					line.addClass("valid");
+					th.off('click mouseenter mouseleave', elem1);
+					th.off('click mouseenter mouseleave', elem2);
+					function elemPosition(elem) {
+						let parent = elem.parent();
+						let elemH = elem.height();
+						let elemTop = parent.height() - elemH;
+						parent.height(parent.height());
+
+						let promise = new Promise(function (resolve, reject) {
+							line.css({
+								width: 0,
+							})
+							elem.after(`<div class="remove-div" style="height:${elemH}px; margin-bottom:1rem"></div>`);
+							elem.css({
+								"top": elem.position().top,
+								"position": "absolute",
+								"z-index": "10",
+							});
+						})
+						function getDown() {
+							elem.animate({
+								"top": elemTop - 16,
+							}, 1000);
+							return false;
+						}
+						promise.then(getDown());
+						promise.then($(".remove-div").animate({ "height": 0, "margin-bottom": 0 }, 1000, function () {
+							elem.removeAttr('style').appendTo(parent);
+							if (elem.hasClass("input-variant--left")) {
+
+								line.css({
+									width: 100,
+									'-webkit-transform': 'rotate(' + 0 + 'deg)',
+									'-moz-transform': 'rotate(' + 0 + 'deg)',
+									'-ms-transform': 'rotate(' + 0 + 'deg)',
+									'-o-transform': 'rotate(' + 0 + 'deg)',
+									'transform': 'rotate(' + 0 + 'deg)',
+								})
+							}
+
+						}));
 
 
-	})
-	// /соеденить блоки линией
+
+					};
+
+					setTimeout(() => {
+						elemPosition(elem1);
+						elemPosition(elem2);
+					}, 500);
+				}
+				else {
+					elem1.addClass(invalideCLass);
+					elem2.addClass(invalideCLass);
+					line.addClass("invalid");
+					elem2.siblings().addClass('disabled');
+					setTimeout(() => {
+						elem1.removeClass(invalideCLass);;
+						elem2.siblings().removeClass('disabled');
+						elem2.removeClass(invalideCLass).removeClass("active ");
+						elem2.siblings().removeClass('disabled');
+						line.removeClass("invalid").attr('style', '');
+						flag = true;
+						// reset(line);
+					}, 1000);
+				}
+
+			}
+
+
+		})
+		// /соеденить блоки линией
 	}
 
 	// customScroll() {
@@ -429,9 +429,9 @@ const JSCCommon = {
 	// 	});
 	// }
 };
- 
 
-$( document ).ready( function()  {
+
+$(document).ready(function () {
 	// полифил для object-fit
 	objectFitImages();
 	// Picture element HTML5 shiv
@@ -451,14 +451,14 @@ $( document ).ready( function()  {
 	JSCCommon.ie();
 	JSCCommon.tooltip();
 	JSCCommon.player();
-	JSCCommon.dragDrop(); 
+	JSCCommon.dragDrop();
 	JSCCommon.sortWords();
 	JSCCommon.sortBlock();
 	// JSCCommon.customScroll();
 
 	// JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
-	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/10.png);"></div>')
+	// $(".main-wrapper").after('<div class="screen" style="background-image: url(screen/10.png);"></div>')
 	// /добавляет подложку для pixel perfect
 
 
@@ -488,9 +488,9 @@ $( document ).ready( function()  {
 	}
 
 	$(window).resize(function () {
-		heightses(); 
+		heightses();
 	});
- 
+
 	heightses();
 
 	// листалка по стр
@@ -505,7 +505,7 @@ $( document ).ready( function()  {
 
 
 	// кнопка показать еще
-	$(".load-more").click(function(){
+	$(".load-more").click(function () {
 		$(this).hide().parent().find('.test-item:hidden').css('display', 'block');
 	});
 
@@ -538,8 +538,7 @@ $( document ).ready( function()  {
 	$(".accordion__toggle").click(function () {
 		$(this).toggleClass("active").next().slideToggle().toggleClass("active");
 	})
- 
- 
+
+
 
 });
- 
